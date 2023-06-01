62ad4f1119c0da9b7c586a308c3205723c95cc54
januszoles <janusz.oles@gmail.com>
2023-06-01 10:47:10 +0000

    fix-api-repos.sh

---
+++ bin/fix-api-repos.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# fix /etc/apk/repositories
+
+echo "==> Change to Alpine Linux repositories"
+echo "--- Old /etc/apk/repositories ---"
+cat /etc/apk/repositories
+echo "---"
+echo https://dl-cdn.alpinelinux.org/alpine/v3.14/main >> /etc/apk/repositories
+echo https://dl-cdn.alpinelinux.org/alpine/v3.14/community >> /etc/apk/repositories
+sed -i -e '/http:\/\/apk.ish.app/d' /etc/apk/repositories
+echo "--- New /etc/apk/repositories ---"
+cat /etc/apk/repositories
+echo "---"
