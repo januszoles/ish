0bc2c3b841c777914895cc8a4254ddef6c49a67d
januszoles <janusz.oles@gmail.com>
2023-06-08 19:47:21 +0200

    typos and more apps

--- bin/reinstall-core-ish-apps-part1.sh
+++ bin/reinstall-core-ish-apps-part1.sh
@@ -4,7 +4,7 @@
 
 #  description : Reinstall and setup core Alpine Linux apps (iSH.app iOS)
 #       author : Janusz Oles
-#      version : 20230607-03
+#      version : 20230608-03
 #        usage : sh /root/bin/reinstall-core-ish-apps-part1.sh
 #         NOTE : need your interaction to set up root password
 #                timezone is set to Europe/Warsaw
@@ -13,78 +13,99 @@
 ##### EDIT! 
 GIT_USER="yourname"
 GIT_EMAIL="youremail@example.com"
-GIT_DEFAULT_BRANCH="main"            #  master|main|whatever
+GIT_DEFAULT_BRANCH="main"            #  change to master|main|whatever
 GIT_CORE_EDITOR="nvim"               #! for now workos only with nvim
-TIMEZONE="/Europe/Warsaw"            # change to your location
+TIMEZONE="/Europe/Warsaw"            #  change to your location
 #
 # Central European Summer Time (CEST) is 2 hours ahead of 
 # Coordinated Universal Time (UTC).
 #####################################################################
 
 ## DEFINE COLORS
-bold='\033[1m'
-black='\033[0;30m'
-red='\033[0;31m'
-green='\033[0;32m'
-yellow='\033[0;33m'
-blue='\033[0;34m'
-magenta='\033[0;35m'
-cyan='\033[0;36m'
-white='\033[0;37m'
-reset='\033[0m'
+_color() {
+  bold='\033[1m'
+  black='\033[0;30m'
+  red='\033[0;31m'
+  green='\033[0;32m'
+  yellow='\033[0;33m'
+  blue='\033[0;34m'
+  magenta='\033[0;35m'
+  cyan='\033[0;36m'
+  white='\033[0;37m'
+  reset='\033[0m'
+}
 
 _info(){
-  printf "%b\n" "${blue}==> $1${reset}"
+  local msg="$1"
+  printf "%b\n" "${blue}==> ${msg}${reset}"
 }
 
 _error(){
-  printf "%b\n" "${red}[!] $1${reset}"
+  local msg="$1"
+  printf "%b\n" "${red}[!] ${msg}${reset}"
 }
-
 
 _div(){
   printf "%s\n" "---"
 }
 
+_change_repo() {
+  _info "Default repo locations"
+  cat /etc/apk/repositories
+  _div
+  # append new lines
+  _info "Change to Alpine Linux repositories"
+  echo https://dl-cdn.alpinelinux.org/alpine/v3.14/main >> /etc/apk/repositories
+  echo https://dl-cdn.alpinelinux.org/alpine/v3.14/community >> /etc/apk/repositories
+  # rem old lines
+  sed -i -e '/http:\/\/apk.ish.app/d' /etc/apk/repositories 
+  # check
+  _info "New repo locations"
+  cat /etc/apk/repositories
+  _div
+}
 
-_info "Default repo locations"
-cat /etc/apk/repositories
-_div
-# append new lines
-_info "Change to Alpine Linux repositories"
-echo https://dl-cdn.alpinelinux.org/alpine/v3.14/main >> /etc/apk/repositories
-echo https://dl-cdn.alpinelinux.org/alpine/v3.14/community >> /etc/apk/repositories
-# rem old lines
-sed -i -e '/http:\/\/apk.ish.app/d' /etc/apk/repositories 
-
-# check
-_info "New repo locations"
-cat /etc/apk/repositories
-_div
-
-
-_info "Update and upgrade from new repo"
-apk update
-apk upgrade
+_update() {
+  _info "Update and upgrade from new repo"
+  apk update
+  apk upgrade
+}
 
 _add_man() {
-_info "Add man pages"
-apk add man-pages
-apk add mandoc-apropos
+  _info "Add man pages"
+  apk add man-pages
+  apk add mandoc-apropos
 }
+
 
 
 _add_nvim() {
   _info "Add nvim"
-  apk add nvim
-  apk add nvim-doc
+  apk add neovim
+  apk add neovim-doc
 }
 
+_add_git() {
+  _info "Add version control system"
+  apk add git
+  apk add git-doc     # git man pages
+}
+
+_add_openssh() {
+  _info "Add SSH server" 
+  apk add openssh
+}
+
+_add_openrc() {
+  _info "Add OpenRC to manage sshd start" 
+  apk add openrc     
+}
 
 _set_timezone() {
-  _info "Add timezone data and set my timezone to ${TIMEZONE}"
+  _info "Add timezone data and set timezone to ${TIMEZONE}"
   date
   apk add tzdata 
+
   # check if 'tzdata' is installed 
   APP_TZDATA=`which tzdata`
   if [ -z "${APP_TZDATA}" ];then  
@@ -98,18 +119,27 @@ _set_timezone() {
       exit 1
   fi
 
-
   cp /usr/share/zoneinfo/"${TIMEZONE}" /etc/localtime
   date
   apk del tzdata
   _div
 }
 
-_add_git() {
-  _info "Add version control system"
-  apk add git         # version control system  
-  apk add git-doc     # git man pages
+_add_core_apps() {
+  apk add curl
+  apk add tree
+  apk add awake
+  apk add rsync
+  apk add tmux
+  apk add bash
+  apk add stow
+  apk add stow-doc
+  apk add fd
+  apk add fzf
+  apk add fzf-tmux
+  apk add ncurses
 }
+
 
 _set_git() {
   _info "Set up git"
@@ -154,17 +184,6 @@ _set_git() {
   _div
 }
 
-_add_openssh() {
-  _info "Add and set-up SSH server (https://www.openssh.com/portable.html)"
-  apk add openssh     # ssh server
-}
-
-
-_add_openrc() {
-  _info "Add OpenRC to manage sshd start" 
-  apk add openrc     
-}
-
 _edit_inittab() {
   _info "Edit /etc/inittab"
   _info "::sysinit:/sbin/openrc sysinit --> ::sysinit:/sbin/openrc"
@@ -178,19 +197,18 @@ _edit_inittab() {
   # Use sed to replace the first instance of the line in the file
   sed -i -e ":a" -e  "\$!{N;ba" -e "}; s|$s|$r|" /etc/inittab
   _div
-  cat -n /etc/inittab |head -n3
+
   _info "Comment out the tty's, there is no access to them, waist of memory."
   _div
-  cat -n /etc/inittab |awk 'NR >= 9 && NR <= 14'
+  cat -n /etc/inittab | grep 'tty[0-9]'
   sed -i -r 's/^(tty[[:digit:]])/# \1/' /etc/inittab
   _div
-  cat -n /etc/inittab |awk 'NR >= 9 && NR <= 14'
+  cat -n /etc/inittab | grep 'tty[0-9]'
   _div
-
 }
 
 _set_ssh() {
-  _info "Set-up SSH server (https://www.openssh.com/portable.html)"
+  _info "Set-up SSH server"
   
   _info "Set the password for root login"
   passwd
@@ -203,19 +221,30 @@ _set_ssh() {
   cat -n /etc/ssh/sshd_config |tail -2
  _div
 }
-# cat /etc/inittab
-_add_man
-_add_nvim
-_add_git
-_add_openssh
-_add_openrc
 
-_set_timezone
-_set_git
-_set_ssh
-_edit_inittab
+_main() {
+  _color
+ 
+  _change_repo
+  _update
+
+  _add_man
+  _add_nvim
+  _add_git
+  _add_openssh
+  _add_openrc
+
+  _add_core_apps 
+
+  _set_timezone
+  _set_git
+  _set_ssh
+  _edit_inittab
+}
+
+_main
+
 _info "Need to restart ish before adding sshd to OpenRC"
+
 exit
-##### END OF SSH SET-UP ######################################################
-##### END OF reinstall-core-ish-apps-part1.sh ################################
-##############################################################################
+
